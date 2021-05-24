44d1899943420db935dd4cbab9bebb5756015d46
Crowdbotics <team@crowdbotics.com>
2021-05-24 13:12:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hussl_27321/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hussl_27321;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "hussl_27321";
+  }
+}
